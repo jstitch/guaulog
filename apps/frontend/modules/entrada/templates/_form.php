8a9347a1304cc2a0a94634643ef30c22ea377fd3
Javier Novoa C <jstitch@invernalia.homelinux.net>
2011-03-23 14:01:30 -0600

    comienzo rediseño de formularios

--- apps/frontend/modules/entrada/templates/_form.php
+++ apps/frontend/modules/entrada/templates/_form.php
@@ -9,11 +9,44 @@
   <?php echo form_tag_for($form, '@entrada') ?>
 <?php endif; ?>
   <table class="entrada_form">
+    <tbody>
+     <?php echo $form ?>
+      <!--tr>
+        <td colspan="2"><div class="error"><?php echo $form->renderGlobalErrors() ?></div></td>
+      </tr>
+      <tr>
+        <td colspan="2"><div class="error"><?php echo $form['mes']->renderError() ?></div></td>
+        <td colspan="2"><div class="error"><?php echo $form['anio']->renderError() ?></div></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th class="label right"><?php echo $form['mes']->renderLabel() ?></th>
+        <td class="field left"><?php echo $form['mes'] ?></td>
+      </tr>
+      <tr>
+        <th class="label right"><?php echo $form['anio']->renderLabel() ?></th>
+        <td class="field left"><?php echo $form['anio'] ?></td>
+      </tr>
+      <tr>
+        <th class="label right"><?php echo $form['mide']->renderLabel() ?></th>
+        <td class="field left"><?php echo $form['mide'] ?></td>
+      </tr>
+      <tr>
+        <th class="label right"><?php echo $form['pesa']->renderLabel() ?></th>
+        <td class="field left"><?php echo $form['pesa'] ?></td>
+      </tr>
+      <tr>
+        <th class="label right"><?php echo $form['pc']->renderLabel() ?></th>
+        <td class="field left"><?php echo $form['pc'] ?></td>
+        <input type="hidden" name="guaulog_entrada[_csrf_token]" value="<?php /*echo $form->getCSRFToken()*/ ?>" id="guaulog_entrada__csrf_token" /-->
+      </tr>
+    </tbody>
+
     <tfoot>
       <tr>
         <td class="right">
           <input type="submit" value="<?php echo 'OK' ?>" class="showButton" />
-          </form>
+</form>
         </td>
         <td class="left">
           <?php if (isset($entrada)): ?>
@@ -26,8 +59,4 @@
         </td>
       </tr>
     </tfoot>
-
-    <tbody>
-      <?php echo $form ?>
-    </tbody>
   </table>
